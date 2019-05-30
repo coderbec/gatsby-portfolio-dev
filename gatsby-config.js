1f7ac092b02aef4d866d43ec01ce0145ef29c19b
Bec Martin <bec.martin@messagemedia.com.au>
2019-05-30 12:19:35 +1000

    akjsdhfaksjdf

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,10 +31,10 @@ module.exports = {
 				// Url to query from
 				url: "https://api.github.com/graphql",
 				// HTTP headers
-				// headers: {
-				//   // Learn about environment variables: https://gatsby.dev/env-vars
-				//   Authorization: `bearer ${process.env.GITHUB_TOKEN}`,
-				// },
+				headers: {
+				  // Learn about environment variables: https://gatsby.dev/env-vars
+				  Authorization: `bearer ${process.env.GITHUB_TOKEN}`,
+				},
 				// Additional options to pass to node-fetch
 				fetchOptions: {},
 			  },
